8844eb994129248ec92551757c257ba04f8d6892
CrisJumbo <tonn.raus09@gmail.com>
2024-11-23 22:46:15 +0100

    FP-UNED pec3 complete

--- fp/pec3/pec3.c
+++ fp/pec3/pec3.c
@@ -1,6 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
+    int year;
+    char month[10];
+    int selectedMonth;
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    char monthInYear[12][20] = {"january","february","march","april","may","june","july","august","september","october","november","december"};
+    char daysInWeek[7][1] = {"L","M","X","J","V","S","D"};
+    int days;
+    int firstDay = 0;
+    int isLeap = 0;
+
+    printf("Year: ");
+    scanf("%d", &year);
+    printf("Month: ");
+    scanf("%s", month);
+
+    if (year < 1601 || year > 3000) {
+        return 0;
+    }
+
+    // Days till 31th of December of the selected year
+    for(int i = 1601; i <= year; i++) {
+        if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+            days += 366;
+        } else {
+            days += 365;
+        }
+    }
+
+    // Days till 31th of December of previous year
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        isLeap = 1;
+        days -= 366;
+    } else {
+        days -= 365;
+    }
+
+    // Days till the selected month
+    for(int i = 0; i < 12; i++) {
+        if (strcmp(month, monthInYear[i]) == 0) {
+            selectedMonth = i;
+            break;
+        }
+        
+    };
+    for(int i = 0; i <= selectedMonth; i++) {
+        if(isLeap && i == 1) {
+            days += daysInMonth[i] + 1;
+        } else {
+            days += daysInMonth[i];
+        }
+    }
+
+    // Days till previous day of the selected month
+    if (isLeap && selectedMonth == 1) {
+        days -= daysInMonth[selectedMonth] + 1;
+    } else {
+        days -= daysInMonth[selectedMonth];
+    }
+
+    // Day of the week that the month start
+    char dayOfWeek = daysInWeek[days % 7][0];
+
+    //Print the calendar
+     printf("|  Monday   ||  Tuesday  || Wednesday ||  Thursday ||  Friday   ||  Saturday ||  Sunday   |\n");
+     printf("|-----------||-----------||-----------||-----------||-----------||-----------||-----------|\n");
+    while(firstDay <= daysInMonth[selectedMonth]) {
+        for(int j = 0; j < 7; j++) {
+            if (dayOfWeek == daysInWeek[j][0] && firstDay == 0) {
+                firstDay++;
+                if (firstDay / 10 == 0) {
+                    printf("|     %d     |", firstDay);
+                } else {
+                    printf("|     %d    |", firstDay);
+                }
+            } else {
+                if (firstDay != 0 && firstDay < daysInMonth[selectedMonth]) {
+                    firstDay++;
+                    if (firstDay / 10 == 0) {
+                        printf("|     %d     |", firstDay);
+                    } else {
+                        printf("|     %d    |", firstDay);
+                    }
+                } else {
+                    firstDay++;
+                    printf("|     *     |");
+                }
+            }
+        };
+        printf("\n");
+    };
+
     return 0;
 };
--- fp/pec3/pec3.md
+++ fp/pec3/pec3.md
@@ -5,3 +5,13 @@
 2. Select Year between range [1601, 3000]
 3. Print the month page for that selection
 
+### Steps
+1. Ask for a year and month.
+2. Condition; if out of range (1601 - 3000).
+3. Condition; if out of range (1 - 12).
+4. Calculate days looping throuh each year from 1601 to introduced year X.
+5. Sum 365, but if year is leap sum 366, then substract 365 or 366 of X year.
+6. Loop till introduced month Y
+7. Add 1 day if indexed February month is in a leap year.
+8. Create 4 weeks calendar or 5 weeks if total days N % 7 is Monday or not.
+9. Print day or empty day "*" for the calendar.
