1fd0e3c15bb03b0f05ee16fbe57cbcca0bcaafb5
CrisJumbo <tonn.raus09@gmail.com>
2024-12-11 14:36:25 +0100

    FP-UNED reduce code block for pec 3 c

--- fp/pec3/pec3.c
+++ fp/pec3/pec3.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+int isLeapYear(int year);
+void printDay(int dayWeek, int dayNumber);
+void printTwoDigitDay(int dayWeek, int dayNumber);
+void printCalendarHeader(char *month, int year);
+void printEmptyDay(int dayWeek);
+
+typedef enum dayType {Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo};
+typedef enum monthType {Enero, Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto, Septiembre, Octubre, Noviembre, Diciembre};
+
+
 int main() {
     int year;
     char month[10];
     int selectedMonth;
     int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    char monthInYear[12][20] = {"january","february","march","april","may","june","july","august","september","october","november","december"};
+    char monthInYear[12][20] = {"JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"};
     char daysInWeek[7][1] = {"L","M","X","J","V","S","D"};
     int days;
     int firstDay = 0;
@@ -17,6 +27,7 @@ int main() {
     scanf("%s", month);
     printf("¿Año (1601..3000)?");
     scanf("%d", &year);
+    isLeap = isLeapYear(year);
 
     if (year < 1601 || year > 3000) {
         return 0;
@@ -32,7 +43,7 @@ int main() {
     }
 
     // Days till 31th of December of previous year
-    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+    if (isLeap == 1) {
         isLeap = 1;
         days -= 366;
     } else {
@@ -74,6 +85,105 @@ int main() {
         weeks = ((days % 7) + daysInSelectedMonth ) / 7 + 1;
     }
 
+
+    printCalendarHeader(month, year);
+    for(int i = 0; i < weeks; i++) {
+        for(int j = 0; j < 7; j++) {
+            if (dayOfWeek == daysInWeek[j][0] && firstDay < daysInSelectedMonth) {
+                firstDay++;
+                if (firstDay / 10 == 0) {
+                    printDay(j, firstDay);
+                } else {
+                    printTwoDigitDay(j,firstDay);
+                }
+            } else {
+                if (firstDay != 0 && firstDay < daysInSelectedMonth) {
+                firstDay++;
+                    if (firstDay / 10 == 0) {
+                        printDay(j, firstDay);
+                    } else {
+                        printTwoDigitDay(j,firstDay);
+                    }
+                } else {
+                    printEmptyDay(j);
+                }
+            }
+        };
+        printf("\n");
+    };
+
+    return 0;
+};
+
+int isLeapYear(int year) {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void printDay(int dayWeek, int dayNumber) {
+    switch(dayWeek) {
+        case 0:
+            printf(" %d", dayNumber);
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            printf("   %d", dayNumber);
+            break;
+        case 5:
+            printf(" |  %d", dayNumber);
+            break;
+        case 6:
+            printf("   %d", dayNumber);
+            break;
+    }
+}
+
+void printTwoDigitDay(int dayWeek, int dayNumber) {
+    switch(dayWeek) {
+        case 0:
+            printf("%d", dayNumber);
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            printf("  %d", dayNumber);
+            break;
+        case 5:
+            printf(" | %d", dayNumber);
+            break;
+        case 6:
+            printf("  %d", dayNumber);
+            break;
+    }
+}
+
+void printEmptyDay(int dayWeek) {
+    switch(dayWeek) {
+        case 0:
+            printf(" .");
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            printf("   .");
+            break;
+        case 5:
+            printf(" |  .");
+            break;
+        case 6:
+            printf("   .");
+            break;
+    }
+};
+
+void printCalendarHeader(char *month, int year) {
     int total_width = 28;
     int month_length = strlen(month);
     int year_length = snprintf(NULL, 0, "%d", year);
@@ -88,110 +198,4 @@ int main() {
      printf("===========================\n");
      printf("LU  MA  MI  JU  VI | SA  DO\n");
      printf("===========================\n");
-    for(int i = 0; i < weeks; i++) {
-        for(int j = 0; j < 7; j++) {
-            if (dayOfWeek == daysInWeek[j][0] && firstDay < daysInSelectedMonth) {
-                firstDay++;
-                if (firstDay / 10 == 0) {
-                    switch(j) {
-                        case 0:
-                            printf(" %d", firstDay);
-                            break;
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                            printf("   %d", firstDay);
-                            break;
-                        case 5:
-                            printf(" |  %d", firstDay);
-                            break;
-                        case 6:
-                            printf("   %d", firstDay);
-                            break;
-                    }
-                } else {
-                    switch(j) {
-                        case 0:
-                            printf("%d", firstDay);
-                            break;
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                            printf("  %d", firstDay);
-                            break;
-                        case 5:
-                            printf(" | %d", firstDay);
-                            break;
-                        case 6:
-                            printf("  %d", firstDay);
-                            break;
-                    }
-                }
-            } else {
-                if (firstDay != 0 && firstDay < daysInSelectedMonth) {
-                firstDay++;
-                    if (firstDay / 10 == 0) {
-                        switch(j) {
-                            case 0:
-                                printf(" %d", firstDay);
-                                break;
-                            case 1:
-                            case 2:
-                            case 3:
-                            case 4:
-                                printf("   %d", firstDay);
-                                break;
-                            case 5:
-                                printf(" |  %d", firstDay);
-                                break;
-                            case 6:
-                                printf("   %d", firstDay);
-                                break;
-                        }
-                    } else {
-                        switch(j) {
-                            case 0:
-                                printf("%d", firstDay);
-                                break;
-                            case 1:
-                            case 2:
-                            case 3:
-                            case 4:
-                                printf("  %d", firstDay);
-                                break;
-                            case 5:
-                                printf(" | %d", firstDay);
-                                break;
-                            case 6:
-                                printf("  %d", firstDay);
-                                break;
-                        }
-                    }
-                } else {
-                    switch(j) {
-                        case 0:
-                            printf(" .");
-                            break;
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                            printf("   .");
-                            break;
-                        case 5:
-                            printf(" |  .");
-                            break;
-                        case 6:
-                            printf("   .");
-                            break;
-                    }
-                }
-            }
-        };
-        printf("\n");
-    };
-
-    return 0;
 };
