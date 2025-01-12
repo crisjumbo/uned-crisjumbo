5bcfa12377c1604d045fd142918d7904a839c503
CrisJumbo <tonn.raus09@gmail.com>
2025-01-13 00:54:00 +0100

    FP-UNED add corrected pec2

--- fp/pec2/pec2.c
+++ fp/pec2/pec2.c
@@ -1,71 +1,93 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+
+char pattern(int pos) {
+  switch(pos) {
+    case 0:
+      return'@';
+      break;
+    case 1:
+      return'.';
+      break;
+    case 2:
+      return'o';
+      break;
+    default:
+      return'.';
+  }
+}
 
 int main() {
     int side;
     int boxSide;
-    char pattern[] = {'@', '.', 'o', '.'};
-    int patternLength = sizeof(pattern);
-    
+    int patternLength;
+    int row;
+    int column;
+    patternLength = 4;
+
     printf("Side of your rhombus: \n");
     scanf("%d", &side);
-    
+
     if (side < 1 || side > 20) {
         return 0;
     }
     boxSide = (side * 2) - 1;
-    int row = 0;
-    
+    row = 0;
+
     while(row < boxSide) {
-        int column = 0;
+        column = 0;
         while(column < boxSide){
             if (row <= side - 1) {
 
                 if ((row + column) >= (side - 1) && (column - row) < side) {
-                
+
                     if (column == side - 1) {
-                        
-                        printf("%c", pattern[row % patternLength]);
-                        
+
+                        printf("%c", pattern(row % patternLength));
+
                     }else if (row + column >= side -1 && column < side) {
 
-                        printf("%c", pattern[(row + column - (side - 1)) % patternLength]);
+                        printf("%c", pattern((row + column - (side - 1)) % patternLength));
 
                     } else {
-                        printf("%c", pattern[((side - 1) - (column - row)) % patternLength]);
-                        
+                        printf("%c", pattern(((side - 1) - (column - row)) % patternLength));
+
                     }
-                    
-                    
+
+
                 } else {
-                    printf(" ");
-                    
+                    if((column - row) < side) {
+                      printf(" ");
+                    }
                 }
 
             } else {
                 if (row - column < side && row + column <= (boxSide + side - 2)) {
 
                     if (column == side - 1) {
-                        printf("%c", pattern[((boxSide + side - 2) - (row + column)) % patternLength]);
+                        printf("%c", pattern(((boxSide + side - 2) - (row + column)) % patternLength));
                     } else if (row - column < side && column < side) {
-                        printf("%c", pattern[((side - 1) - (row - column)) % patternLength]);
+                        printf("%c", pattern(((side - 1) - (row - column)) % patternLength));
                     }
                     else {
-                        printf("%c", pattern[((boxSide + side - 2) - (row + column)) % patternLength]);
+                        printf("%c", pattern(((boxSide + side - 2) - (row + column)) % patternLength));
                     }
 
                 } else {
+                  if(row + column <= (boxSide + side - 2)){
                     printf(" ");
+                  }
                 }
 
             }
-            
+
             column++;
         }
         row++;
         printf("\n");
     }
-    
-    
+
+
     return 0;
 }
